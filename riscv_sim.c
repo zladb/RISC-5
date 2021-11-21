3b789076c1964109b37bbcd4661ce3733427f69b
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-11-21 17:14:31 +0900

    Update riscv_sim.c
    add, addi 구현

--- riscv_sim.c
+++ riscv_sim.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 //clock cycles
 long long cycles = 0;
@@ -27,43 +28,190 @@ void print_cycles();
 void print_reg();
 void print_pc();
 
-char opcode[7];
-char funct3[3];
-char funct7[7];
-char rd[5];
-char rs1[5];
-char rs2[5];
-char imm[12];
+char inst[33];
+char inst32[33];
+char opcode[8];
+char funct3[4];
+char funct7[8];
+char rd[6];
+char rs1[6];
+char rs2[6];
+char I_imm[13];
+char S_imm[13];
 
-int rs1;
-int rs2;
+int memto_reg;
+int reg_write;
+int mem_read;
+int mem_write;
+int branch;
 
-char ALUOP[2];
+int rd_value;
+int rs1_value;
+int rs2_value;
+int imm_value;
+int ALUresult;
+
+char type[10];
+
+int read_bin(char code[]) 
+{
+	int result = 0;
+	int code_len = strlen(code);
+	for (int i = 0; code[i]; i++) {
+		result = (result << 1) + code[i] - '0';
+	}
+	return result;
+}
 
 //fetch an instruction from a instruction memory
 void fetch() {
 
+	_itoa(inst_mem[pc], inst, 2);
+
+	memset(inst32, '0', 33);
+	memset(opcode, '\0', 8);
+	memset(funct3, '\0', 4);
+	memset(funct7, '\0', 8);
+	memset(rd, '\0', 6);
+	memset(rs1, '\0', 6);
+	memset(rs2, '\0', 6);
+	memset(I_imm, '\0', 13);
+	memset(S_imm, '\0', 13);
+
+	int len = strlen(inst);
+	strcpy(inst32 + (32 - len), inst);
+	printf("%s\n", inst32);
+
+	strncpy(opcode, inst32 + 25, 7);
+	printf("opcode:	%s\n", opcode);
+
+	strncpy(rd, inst32 + 20, 5);
+	printf("rd: %s\n", rd);
+
+	strncpy(funct3, inst32 + 17, 3);
+	printf("funct3: %s\n", funct3);
+
+	strncpy(rs1, inst32 + 12, 5);
+	printf("rs1: %s\n", rs1);
+
+	strncpy(rs2, inst32 + 7, 5);
+	printf("rs2: %s\n", rs2);
+
+	strncpy(funct7, inst32, 7);
+	printf("funct7: %s\n", funct7);
+
+	strcat(I_imm, funct7);
+	strcat(I_imm, rs2);
+	printf("I_imm: %s\n", I_imm);
+
+	strcat(S_imm, funct7);
+	strcat(S_imm, rd);
+	printf("S_imm: %s\n", S_imm);
+
+	pc++;
 }
 
 //decode the instruction and read data from register file
 void decode() {
 	// add, addi, beq, jal, jalr, sd, ld
-	if (strcmp(opcode, 0110011) == 0) {
-		if (funct7 == 0000000) add
-		else if (fucnt7 == 0100000) sub
+	if (strcmp(opcode, "0110011") == 0 && strcmp(funct7, "0000000") == 0)
+	{
+		//add
+		rd_value = read_bin(rd);
+		printf("%d\n", rd_value);
+
+		rs1_value = read_bin(rs1);
+		printf("%d\n", rs1_value);
+
+		rs2_value = read_bin(rs2);
+		printf("%d\n", rs2_value);
+
+		strcpy(type, "add\0");
+		memto_reg = 0;
+		reg_write = 1;
+		mem_read = 0;
+		mem_write = 0;
+		branch = 0;
 	}
 
-	if (strcmp(opcode, 0010011) == 0 && funct3 == 000) addi
+	if (strcmp(opcode, "0010011") == 0 && strcmp(funct3, "000") == 0)
+	{
+		//addi
+		rd_value = read_bin(rd);
+		printf("%d\n", rd_value);
 
-	if (strcmp(opcode, 0000011) == 0 && funct3 == 011) ld
+		rs1_value = read_bin(rs1);
+		printf("%d\n", rs1_value);
 
-	if (strcmp(opcode, 0100011) == 0) sd 
+		imm_value = read_bin(I_imm);
+		printf("%d\n", imm_value);
 
+		strcpy(type, "addi\0");
+		memto_reg = 0;
+		reg_write = 1;
+		mem_read = 0;
+		mem_write = 0;
+		branch = 0;
+	}
+
+	// if (strcmp(opcode, 1100011) == 0 && funct3 == 000) beq;
+
+	//if (strcmp(opcode, 1101111) == 0) jal;
+
+	//if (strcmp(opcode, 1100111) == 0 && funct3 == 000) jalr;
+
+	if (strcmp(opcode, "0000011") == 0 && strcmp(funct3, "011") == 0) 
+	{
+		// ld
+		rd_value = read_bin(rd);
+		printf("%d\n", rd_value);
+
+		rs1_value = read_bin(rs1);
+		printf("%d\n", rs1_value);
+
+		imm_value = read_bin(I_imm);
+		printf("%d\n", imm_value);
+
+		strcpy(type, "ld\0");
+		memto_reg = 1;
+		reg_write = 1;
+		mem_read = 1;
+		mem_write = 0;
+		branch = 0;
+	}
+
+	if (strcmp(opcode, "0100011") == 0)
+	{
+		//sd
+
+		rs1_value = read_bin(rs1);
+		printf("%d\n", rs1_value);
+
+		imm_value = read_bin(S_imm); // address
+		printf("%d\n", imm_value);
+
+		rs2_value = read_bin(rs2); // 저장할 register
+		printf("%d\n", rs2_value);
+
+		strcpy(type, "ld\0");
+		reg_write = 0;
+		mem_read = 0;
+		mem_write = 1;
+		branch = 0;
+	}
 }
 
 //perform the appropriate operation 
 void exe() {
+	if (strcmp(type, "add") == 0)
+	{
+		ALUresult = regs[rs1_value] + regs[rs2_value];
+	}
 
+	if (strcmp(type, "addi") == 0)
+	{
+		ALUresult = regs[rs1_value] + imm_value;
+	}
 }
 
 //access the data memory
@@ -73,7 +221,13 @@ void mem() {
 
 //write result of arithmetic operation or data read from the data memory if required
 void wb() {
+	if(memto_reg == 0 && reg_write == 1)  //R
+		regs[rd_value] = ALUresult;
 
+	if (memto_reg == 1 && reg_write == 1) //ld
+		regs[rd_value] = data_mem[10];
+
+	regs[0] = 0;
 }
 
 int main(int ac, char* av[])
@@ -90,16 +244,15 @@ int main(int ac, char* av[])
 	if (init(av[1]) != 0)
 		return -1;
 
-	char arr[100];
 
 	printf("inst_mem[0] = %d\n", inst_mem[0]);
-	printf("inst_mem[0] to bin = %s\n", _itoa(inst_mem[0], arr, 2));
-	printf("inst_mem[1] = %d\n", inst_mem[1]);
-	printf("inst_mem[0] to bin = %s\n", _itoa(inst_mem[1], arr, 2));
+	printf("inst_mem[0] to bin = %32s\n", _itoa(inst_mem[0], inst, 2));
+	//printf("inst_mem[1] = %d\n", inst_mem[1]);
+	/*printf("inst_mem[0] to bin = %s\n", _itoa(inst_mem[1], arr, 2));
 	printf("inst_mem[2] = %d\n", inst_mem[2]);
 	printf("inst_mem[0] to bin = %s\n", _itoa(inst_mem[2], arr, 2));
 	printf("inst_mem[3] = %d\n", inst_mem[3]);
-	printf("inst_mem[0] to bin = %s\n", _itoa(inst_mem[3], arr, 2));
+	printf("inst_mem[0] to bin = %s\n", _itoa(inst_mem[3], arr, 2));*/
 
 	while (!done)
 	{
